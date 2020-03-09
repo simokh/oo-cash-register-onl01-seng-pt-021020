652295f75a638e657cf791558db01061ba7a894a
Simo Khouili <m.simokhouili@gmail.com>
2020-03-09 01:41:36 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -16,10 +16,19 @@ class CashRegister
       total
     end
 
-    def add_item(title, price, quantity = 1)
-      @price = price
-      @total += price + quantity
-    end
+    def add_item(item, price, quantity = 1)
+        @price = price
+        @total += price * quantity
+        if quantity > 1
+          counter = 0
+          while counter < quantity
+            @items << item
+            counter += 1
+          end
+        else
+          @items << item
+        end
+      end
 
 
 end
